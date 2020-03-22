ec13f069ad95c12752ef4207463a8ea6df41736d
SyuJyo <jayong93@gmail.com>
2020-03-22 23:38:12 +0900

    g++ hp set

---
+++ linux_hp_set.cpp
@@ -0,0 +1,356 @@
+#include <mutex>
+#include <thread>
+#include <iostream>
+#include <chrono>
+#include <vector>
+#include <mutex>
+#include <memory>
+#include <atomic>
+
+using namespace std;
+using namespace chrono;
+
+static const int NUM_TEST = 100000;
+static const int RANGE = 1000;
+
+int unsigned long my_next = 1;
+
+int my_rand(){
+    my_next = my_next * 1103515245 + 12345;
+    return (unsigned long)(my_next/65536) % 32768;
+}
+class LFNODE {
+public:
+	int key;
+	volatile unsigned next;
+
+	LFNODE() {
+		next = 0;
+	}
+	LFNODE(int x) {
+		key = x;
+		next = 0;
+	}
+	~LFNODE() {
+	}
+	LFNODE* GetNext() {
+		return reinterpret_cast<LFNODE*>(next & 0xFFFFFFFE);
+	}
+
+	void SetNext(LFNODE* ptr) {
+		next = reinterpret_cast<unsigned>(ptr);
+	}
+
+	LFNODE* GetNextWithMark(bool* mark) {
+		int temp = next;
+		*mark = (temp % 2) == 1;
+		return reinterpret_cast<LFNODE*>(temp & 0xFFFFFFFE);
+	}
+
+	bool CAS(int old_value, int new_value)
+	{
+		return atomic_compare_exchange_strong(
+			reinterpret_cast<volatile atomic_int*>(&next),
+			&old_value, new_value);
+	}
+
+	bool CAS(LFNODE* old_next, LFNODE* new_next, bool old_mark, bool new_mark) {
+		unsigned old_value = reinterpret_cast<unsigned>(old_next);
+		if (old_mark) old_value = old_value | 0x1;
+		else old_value = old_value & 0xFFFFFFFE;
+		unsigned new_value = reinterpret_cast<unsigned>(new_next);
+		if (new_mark) new_value = new_value | 0x1;
+		else new_value = new_value & 0xFFFFFFFE;
+		return CAS(old_value, new_value);
+	}
+
+	bool TryMark(LFNODE* ptr)
+	{
+		unsigned old_value = reinterpret_cast<unsigned>(ptr) & 0xFFFFFFFE;
+		unsigned new_value = old_value | 1;
+		return CAS(old_value, new_value);
+	}
+
+	bool IsMarked() {
+		return (0 != (next & 1));
+	}
+};
+
+const unsigned int MAX_THREADS = 16;
+thread_local vector<LFNODE*> rlist;
+LFNODE** HPprev[MAX_THREADS];
+LFNODE** HPcurr[MAX_THREADS];
+LFNODE** HPsucc[MAX_THREADS];
+
+int R;
+int num_threads;
+
+
+void scan() {
+	int sz = rlist.size();
+	int last = sz - 1;
+	int idx = 0;
+
+	for (int i = 0; i < sz; ++i) {
+		bool find = false;
+		for (int t = 0; t < num_threads; ++t) {
+			atomic_thread_fence(memory_order_acquire);
+			if (*HPprev[t] == rlist[idx]
+				|| *HPcurr[t] == rlist[idx]
+				|| *HPsucc[t] == rlist[idx]) {
+				find = true;
+				break;
+			}
+		}
+
+		if (!find) {
+			delete rlist[idx];
+
+			rlist[idx] = rlist[last--];
+			rlist.pop_back();
+		}
+		else {
+			++idx;
+		}
+	}
+}
+
+class LFSET
+{
+	LFNODE head, tail;
+public:
+	LFSET()
+	{
+		head.key = 0x80000000;
+		tail.key = 0x7FFFFFFF;
+		head.SetNext(&tail);
+	}
+
+	void retire(LFNODE* old)
+	{
+		rlist.push_back(old);
+		if (rlist.size() >= R) {
+			scan();
+		}
+	}
+
+	void Init()
+	{
+		while (head.GetNext() != &tail) {
+			LFNODE* temp = head.GetNext();
+			head.next = temp->next;
+			delete temp;
+		}
+	}
+
+	void Dump()
+	{
+		LFNODE* ptr = head.GetNext();
+		cout << "Result Contains : ";
+		for (int i = 0; i < 20; ++i) {
+			cout << ptr->key << ", ";
+			if (&tail == ptr) break;
+			ptr = ptr->GetNext();
+		}
+		cout << endl;
+	}
+
+	void Find(int x, LFNODE** pred, LFNODE** curr, int tid)
+	{
+	retry:
+		LFNODE* pr = &head;
+		LFNODE* cu;
+		do {
+			cu = pr->GetNext();
+			*HPcurr[tid] = (cu);
+			atomic_thread_fence(memory_order_release);
+		} while (pr->GetNext() != cu);
+
+		while (true) {
+			bool removed;
+			LFNODE* su = cu->GetNextWithMark(&removed);
+			while (true == removed) {
+				do {
+					su = cu->GetNext();
+					*HPsucc[tid] = (su);
+					atomic_thread_fence(memory_order_release);
+				} while (cu->GetNext() != su);
+
+				if (false == pr->CAS(cu, su, false, false)) {
+					*HPprev[tid] = (nullptr);
+					*HPcurr[tid] = (nullptr);
+					*HPsucc[tid] = (nullptr);
+					atomic_thread_fence(memory_order_release);
+					goto retry;
+				}
+				retire(cu);
+
+				cu = su;
+				*HPcurr[tid] = (su);
+				atomic_thread_fence(memory_order_release);
+				do {
+					su = cu->GetNextWithMark(&removed);
+					*HPsucc[tid] = (su);
+					atomic_thread_fence(memory_order_release);
+				} while (cu->GetNext() != su);
+			}
+			*HPsucc[tid] = (nullptr);
+			atomic_thread_fence(memory_order_release);
+
+			if (cu->key >= x) {
+				*pred = pr; *curr = cu;
+				return;
+			}
+
+			pr = cu;
+			*HPprev[tid] = (cu);
+			atomic_thread_fence(memory_order_release);
+			do {
+				cu = pr->GetNext();
+				*HPcurr[tid] = (cu);
+				atomic_thread_fence(memory_order_release);
+			} while (pr->GetNext() != cu);
+		}
+	}
+	bool Add(int x, int tid)
+	{
+		LFNODE* pred, * curr;
+		LFNODE* e = new LFNODE(x);
+		while (true) {
+			*HPprev[tid] = (nullptr);
+			*HPcurr[tid] = (nullptr);
+			atomic_thread_fence(memory_order_release);
+			Find(x, &pred, &curr, tid);
+
+			if (curr->key == x) {
+				delete e;
+				*HPprev[tid] = (nullptr);
+				*HPcurr[tid] = (nullptr);
+				atomic_thread_fence(memory_order_release);
+				return false;
+			}
+			else {
+				e->SetNext(curr);
+				if (false == pred->CAS(curr, e, false, false))
+					continue;
+				*HPprev[tid] = (nullptr);
+				*HPcurr[tid] = (nullptr);
+				atomic_thread_fence(memory_order_release);
+				return true;
+			}
+		}
+	}
+	bool Remove(int x, int tid)
+	{
+		LFNODE* pred, * curr;
+		while (true) {
+			*HPprev[tid] = (nullptr);
+			*HPcurr[tid] = (nullptr);
+			atomic_thread_fence(memory_order_release);
+			Find(x, &pred, &curr, tid);
+
+			if (curr->key != x) {
+				*HPprev[tid] = (nullptr);
+				*HPcurr[tid] = (nullptr);
+				atomic_thread_fence(memory_order_release);
+				return false;
+			}
+			else {
+				LFNODE* succ = curr->GetNext();
+				if (false == curr->TryMark(succ)) continue;
+				if (pred->CAS(curr, succ, false, false)) {
+					retire(curr);
+				}
+				*HPprev[tid] = (nullptr);
+				*HPcurr[tid] = (nullptr);
+				atomic_thread_fence(memory_order_release);
+				// delete curr;
+				return true;
+			}
+		}
+	}
+	bool Contains(int x, int tid)
+	{
+		LFNODE* prev = &head;
+		LFNODE* curr;
+		do {
+			curr = prev->GetNext();
+			*HPcurr[tid] = (curr);
+			atomic_thread_fence(memory_order_release);
+		} while (prev->GetNext() != curr);
+
+		while (curr->key < x) {
+			prev = curr;
+			*HPprev[tid] = (curr);
+			atomic_thread_fence(memory_order_release);
+			do {
+				curr = prev->GetNext();
+				*HPcurr[tid] = (curr);
+				atomic_thread_fence(memory_order_release);
+			} while (prev->GetNext() != curr);
+		}
+
+		bool ret = (false == curr->IsMarked()) && (x == curr->key);
+		*HPprev[tid] = (nullptr);
+		*HPcurr[tid] = (nullptr);
+		return ret;
+	}
+};
+
+LFSET my_set;
+
+void benchmark(int num_thread, int tid)
+{
+	for (int i = 0; i < NUM_TEST / num_thread; ++i) {
+		//	if (0 == i % 100000) cout << ".";
+		switch (my_rand() % 3) {
+		case 0: my_set.Add(my_rand() % RANGE, tid); break;
+		case 1: my_set.Remove(my_rand() % RANGE, tid); break;
+		case 2: my_set.Contains(my_rand() % RANGE, tid); break;
+		default: cout << "ERROR!!!\n"; exit(-1);
+		}
+	}
+}
+
+int main()
+{
+	vector <thread> worker;
+	for (int i = 0; i < MAX_THREADS; ++i) {
+		HPprev[i] = new LFNODE*;
+		HPcurr[i] = new LFNODE*;
+		HPsucc[i] = new LFNODE*;
+
+	}
+
+	for (int num_thread = 16; num_thread <= 16; num_thread *= 2) {
+		my_set.Init();
+		worker.clear();
+		for (int i = 0; i < MAX_THREADS; ++i) {
+			*HPprev[i] = (nullptr);
+			*HPcurr[i] = (nullptr);
+			*HPsucc[i] = (nullptr);
+			atomic_thread_fence(memory_order_release);
+		}
+		num_threads = num_thread;
+		R = 3 * num_thread * 2;
+
+		auto start_t = high_resolution_clock::now();
+		for (int i = 0; i < num_thread; ++i)
+			worker.push_back(thread{ benchmark, num_thread, i });
+		for (auto& th : worker) th.join();
+		auto du = high_resolution_clock::now() - start_t;
+		my_set.Dump();
+
+
+
+		cout << num_thread << " Threads,  Time = ";
+		cout << duration_cast<milliseconds>(du).count() << " ms\n";
+	}
+
+	for (int i = 0; i < MAX_THREADS; ++i) {
+		delete HPprev[i];
+		delete HPcurr[i];
+		delete HPsucc[i];
+
+	}
+}
